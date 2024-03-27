a628be5ddeb9e1732c3ae7b91eb9f8809398637a
LILIAN ORINA <LORINA@ask.hq>
2024-03-27 21:50:51 +0300

    another

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,3 +1,11 @@
+cars = { "Number Plate":"KDF 1234",
+         "Make": "Mercedes Benz",
+         "Colour": "Burgundy",
+         "Horse Power": "13cc"}
+print(cars["Number Plate"])
+print(cars["Make"])
+print(cars["Colour"])
+print(cars["Horse Power"])
 
 
 
@@ -7,5 +15,3 @@
 
 
 
-
-}
